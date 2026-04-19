87984dd7cdbe04e80ef92b2ab9182201eab4e272
Emmanuel Dadem <emmadadem@gmail.com>
2026-04-19 20:20:02 +0200

    bugfix(shell): fix `run.sh` script

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# Add Qt and CMake to the PATH (Linux)
-export PATH="$HOME/Qt/Tools/CMake/bin:$HOME/Qt/6.11.0/gcc_64/bin:$PATH"
+# Add Qt and CMake to the PATH
+if [[ "$(uname)" == "Darwin" ]]; then
+  export PATH="$HOME/Qt/Tools/CMake/CMake.app/Contents/bin:$HOME/Qt/6.9.1/macos/bin:$PATH"
+else
+  export PATH="$HOME/Qt/Tools/CMake/bin:$HOME/Qt/6.11.0/gcc_64/bin:$PATH"
+fi
 
-cmake -S . -B build && cmake --build build && ./build/LetTalkContentStudio
+cmake -S . -B build && cmake --build build
+
+# Run the executable (macOS uses an app bundle)
+if [[ -d ./build/LetTalkContentStudio.app ]]; then
+  ./build/LetTalkContentStudio.app/Contents/MacOS/LetTalkContentStudio
+else
+  ./build/LetTalkContentStudio
+fi
